602b46ac34e8d071b79c07f287593ba0f5a88aa3
4MN35T <tanmaykarmore01@gmail.com>
2023-03-06 15:22:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+const [name,setName]=useState(false);
+const [inp,setInp]=useState(false);
+function Myfunc(){
+  setInp(name)
+}
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={(e)=>setName(e.target.value)}></input>
+      <button id='button' onClick={Myfunc}>Click</button>
+      <p id='text'> Hello my name is {inp?inp:"__"} and I study at Newton School</p>
     </div>
   )
 }
